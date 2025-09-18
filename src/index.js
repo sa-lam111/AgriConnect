b07b9a25660862f4ec097ebd4577a9163b227b6f
Reclaimed1 <olanijunaid@gmail.com>
2025-09-18 12:32:30 +0100

    confi works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {connect} from 'mongoose';
-import dotenv from 'dotenv';
+import {configDotenv} from 'dotenv';
 import app from "./app.js"
-dotenv.config()
-console.log(process.env.MONGO_URL);
+configDotenv()
+// console.log(process.env.MONGO_URL);
 try{
-    const con=await connect("mongodb+srv://olanijunaid_db_user:uuLL6mF9lNcWRA8L@agriconnect-all.rezwzeu.mongodb.net/?retryWrites=true&w=majority&appName=Agriconnect-all");
+    const con=await connect(process.env.MONGO_URL);
     console.log(`MongoDB connectd: ${con.connection.host}`)
 }
 catch(error){
