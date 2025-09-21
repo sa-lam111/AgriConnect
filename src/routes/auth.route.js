101f638664e362e395432dd9a59aeed7e8578078
Reclaimed1 <olanijunaid@gmail.com>
2025-09-21 15:16:43 +0100

    seeeing if everything never messup

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,16 +1,12 @@
-// import {Router} from 'express';
-// <<<<<<< main
-// import { userRegister,userLogin,farmerRegister,farmerLogin } from '../controllers/auth.controller.js';
-// =======
-// import { register, login } from '../controllers/auth.controller.js';
-// >>>>>>> main
+import {Router} from 'express';
+import { userRegister,userLogin,farmerRegister,farmerLogin } from '../controllers/auth.controller.js';
 
-// const router=Router();
+const router=Router();
 
-// router.post('/uRegister',userRegister);
-// router.post('/uLogin',userLogin);
-// router.post('/fRegister',farmerRegister);
-// router.post('/fLogin',farmerLogin);
+router.post('/uRegister',userRegister);
+router.post('/uLogin',userLogin);
+router.post('/fRegister',farmerRegister);
+router.post('/fLogin',farmerLogin);
 
 
-// export default router;
+export default router;
